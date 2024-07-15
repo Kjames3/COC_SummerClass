4ab5dc8036a0b90ab6c8b0aff6a98c4c8a283785
Kjames3 <bestofkamren@gmail.com>
2024-07-15 12:21:48 -0700

    Made some improvements to remove some errors from the Zybooks testcases. 1 error persist

--- src/Chapter11_Program4.java
+++ src/Chapter11_Program4.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class Chapter11_Program4 {
-    public static void menuMethod() {
-        System.out.println();
-        System.out.println("MENU");
+    public static void printMenu() {
+        System.out.println("\nMENU");
         System.out.println("u - Update player rating");
         System.out.println("a - Output players above a rating");
         System.out.println("r - Replace player");
@@ -13,98 +12,81 @@ public class Chapter11_Program4 {
         System.out.println("Choose an option:");
     }
 
-    public static void outputRoster(int[] players, int[] playerRanks) {
-        for (int i = 0; i < players.length; i++) {
-            System.out.println("Player " + (i + 1) + " -- Jersey number: " + players[i] + ", Rating: " + playerRanks[i]);
-        }
-    }
-
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] jerseyNumber = new int[5];
-        int[] playerRank = new int[5];
-        char choosenCharacter;
+        Scanner scan = new Scanner(System.in);
+        int[] jerseyNumbers = new int[5];
+        int[] ratings = new int[5];
 
-        for (int i = 1; i < jerseyNumber.length + 1; i++) {
-            System.out.println("Enter player" + i + "'s jersey number:");
-            jerseyNumber[i - 1] = scanner.nextInt();
-            System.out.println("Enter player" + i + "'s ranking:");
-            playerRank[i - 1] = scanner.nextInt();
-            System.out.println();
+        // Get the player numbers and ratings
+        for (int i = 0; i < 5; ++i) {
+            System.out.print("Enter player " + (i + 1) + "'s jersey number:");
+            jerseyNumbers[i] = scan.nextInt();
+            System.out.println("Enter player " + (i + 1) + "'s rating:");
+            ratings[i] = scan.nextInt();
         }
+
+        // Print the Rosters and ratings
         System.out.println("ROSTER");
-        outputRoster(jerseyNumber, playerRank);
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseyNumbers[i] + ", Rating: " + ratings[i]);
+        }
 
-        while (true) {
-            menuMethod();
 
-            choosenCharacter = scanner.next().charAt(0);
-            switch(choosenCharacter) {
+        char menuOption;
+        do {
+            printMenu();
+            menuOption = scan.next().charAt(0);
+
+            switch (menuOption) {
+                case 'o':
+                    System.out.println("ROSTER");
+                    for (int i = 0; i < 5; ++i) {
+                        System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseyNumbers[i] + ", Rating: " + ratings[i]);
+                    }
+                    break;
+
                 case 'u':
                     System.out.println("Enter a jersey number:");
-                    int updatePlayerNumber = scanner.nextInt();
-                    int updatePlayerIndex = -1;
-
-                    // Find the index of the jersey number in the jerseyNumber array
-                    for (int i = 0; i < jerseyNumber.length; i++) {
-                        if (jerseyNumber[i] == updatePlayerNumber) {
-                            updatePlayerIndex = i;
-                            break;
+                    int jersey = scan.nextInt();
+                    System.out.println("Enter a new rating for player:");
+                    int rating = scan.nextInt();
+                    for (int i = 0; i < 5; ++i) {
+                        if (jerseyNumbers[i] == jersey) {
+                            ratings[i] = rating;
                         }
                     }
-
-                    // If the jersey number is found, update the corresponding player rank
-                    if (updatePlayerIndex != -1) {
-                        System.out.println("Enter new rating:");
-                        int updateRating = scanner.nextInt();
-                        playerRank[updatePlayerIndex] = updateRating;
-                    } else {
-                        System.out.println("Jersey number not found.");
-                    }
                     break;
+
                 case 'a':
                     System.out.println("Enter a rating:");
-                    int aboveRating = scanner.nextInt();
-                    System.out.println();
-                    System.out.println("ABOVE:");
-                    for (int i = 0; i < playerRank.length; i++) {
-                        if (playerRank[i] > aboveRating) {
-                            System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseyNumber[i] + ", Rating: " + playerRank[i]);
+                    int ratingThreshold = scan.nextInt();
+                    System.out.println("ABOVE " + ratingThreshold);
+                    for (int i = 0; i < 5; ++i) {
+                        if (ratings[i] > ratingThreshold) {
+                            System.out.println("Player " + (i + 1) + " -- Jersey number: " + jerseyNumbers[i] + ", Rating: " + ratings[i]);
                         }
                     }
                     break;
+
                 case 'r':
+                    System.out.println();
                     System.out.println("Enter a jersey number:");
-                    int replacePlayerNumber = scanner.nextInt();
-                    int replacePlayerIndex = -1;
-
-                    // Find the index of the jersey number in the jerseyNumber array
-                    for (int i = 0; i < jerseyNumber.length; i++) {
-                        if (jerseyNumber[i] == replacePlayerNumber) {
-                            replacePlayerIndex = i;
-                            break;
+                    int oldJersey = scan.nextInt();
+                    System.out.println("Enter a new jersey number:");
+                    int newJersey = scan.nextInt();
+                    System.out.println("Enter a rating for the new player:");
+                    int newRating = scan.nextInt();
+                    scan.nextLine();
+                    for (int i = 0; i < 5; ++i) {
+                        if (jerseyNumbers[i] == oldJersey) {
+                            jerseyNumbers[i] = newJersey;
+                            ratings[i] = newRating;
                         }
                     }
-
-                    // If the jersey number is found, prompt the user to enter the new jersey number and rank
-                    if (replacePlayerIndex!= -1) {
-                        System.out.println("Enter new jersey number:");
-                        int newJerseyNumber = scanner.nextInt();
-                        System.out.println("Enter new rating:");
-                        int newRating = scanner.nextInt();
-
-                        jerseyNumber[replacePlayerIndex] = newJerseyNumber;
-                        playerRank[replacePlayerIndex] = newRating;
-                    }
-                    break;
-                case 'o':
-                    outputRoster(jerseyNumber, playerRank);
-                    break;
-                case 'q':
-                    System.exit(0);
                     break;
             }
-        }
+        } while (menuOption != 'q');
+
+        scan.close();
     }
 }
