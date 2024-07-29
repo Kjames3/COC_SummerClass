3798918888ef7a6531e5d81cd6a9a5abdf4266ec
Kjames3 <bestofkamren@gmail.com>
2024-07-29 13:32:11 -0700

    No Progress 80% functional

--- src/Chapter15_Program63.java
+++ src/Chapter15_Program63.java
@@ -6,13 +6,26 @@ public class Chapter15_Program63 {
 
         // Declare and initialize variables
         String userText = scanner.nextLine();
+        int counter = 0;
+        boolean isQuestion = false;
 
+        for (int i = 0; i < userText.length() - 1; i++) {
+            if (userText.charAt(i) == '?') {
+                isQuestion = true;
+            }
+        }
 
-        // TODO: Fix problem here with the ? character
         // Remove spaces, numbers, special characters
         userText = userText.replaceAll("\\s+|\\d+|\\W", "");
+        counter = userText.length();
+
+
+        // Check if the text ends with a question mark
+        if (isQuestion) {
+            counter++;
+        }
 
         // Print the length of the modified text
-        System.out.println(userText.length());
+        System.out.println(counter);
     }
 }
