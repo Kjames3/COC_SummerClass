72cdbd37717fafa60e7378440a62787b2265dcbb
Kjames3 <bestofkamren@gmail.com>
2024-07-29 13:32:57 -0700

    Need to complete this program

---
+++ src/Chapter15_Program67.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Chapter15_Program67 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a character:");
+        char triangleCharacter = scanner.next().charAt(0);
+        System.out.println("Enter triangle height:");
+        int triangleHeight = scanner.nextInt();
+
+        System.out.println();
+
+    }
+}
